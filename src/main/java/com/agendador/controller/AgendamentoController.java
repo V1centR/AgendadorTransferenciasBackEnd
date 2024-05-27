26eba4421dabebc27c1f0bc048407bcd6b11774b
V1centR <vicentcdb@gmail.com>
2024-05-26 23:12:17 -0300

    create controller GET endpoints

--- src/main/java/com/agendador/controller/AgendamentoController.java
+++ src/main/java/com/agendador/controller/AgendamentoController.java
@@ -1,18 +1,42 @@
 package com.agendador.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.agendador.entities.Agendamento;
+import com.agendador.impl.AgendamentoImpl;
+
 
 @RestController
 @RequestMapping("/agendamento")
 public class AgendamentoController {
 	
+	@Autowired
+	AgendamentoImpl service;
+	
+	
+//	@GetMapping("/all")
+//	public List<Agendamento> getAll() {
+//		return service.getAllAgendamento();
+//	}
 	
 	@CrossOrigin
-	@GetMapping("/all")
-	public String getAll() {
-		return "{'OK':'OK'}";
+	@GetMapping("/today")
+	public List<Agendamento> getToday() {
+		return service.getTodayAgendamento();
 	}
+	
+	@CrossOrigin
+	@GetMapping("/range/{startDate}/{endDate}")
+	public List<Agendamento> getByRangeDate(@PathVariable("startDate") String startDate, @PathVariable("endDate") String endDate) {
+		return service.findByRangeDate(startDate,endDate);
+	}
+	
+
 }
