93f0365faaf693041903ccfc18b6525cd5ec5578
V1centR <vicentcdb@gmail.com>
2024-05-26 22:51:41 -0300

    create packages,entities + repo interface

---
+++ src/main/java/com/agendador/repo/AgendamentoRepo.java
@@ -0,0 +1,12 @@
+package com.agendador.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.agendador.entities.Agendamento;
+
+@Repository
+public interface AgendamentoRepo extends JpaRepository<Agendamento, Integer> {
+	
+
+}
